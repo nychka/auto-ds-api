3ee70ed1eb15477440c455be3d1de06f06aed1e6
Oleh Budurovych <oleh_budurovych@epam.com>
2018-04-12 11:05:07 +0300

    Prepare env to stage environment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins "http://10.6.193.162/3000"
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
